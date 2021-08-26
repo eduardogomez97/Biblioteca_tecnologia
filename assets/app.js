9d249ae85d414424816e6931a8fa3d85a998c7a1
eduardogomez97 <ryukedu86@gmail.com>
2021-08-26 22:01:12 +0200

    Estilos y traducción datatables

--- assets/app.js
+++ assets/app.js
@@ -3,15 +3,7 @@ import 'datatables.net';
 import 'datatables.net-bs4';
 
 
-$('#bibliotecas').dataTable( {
-    "language": {
-        "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-    },
-    dom: 'Bfrtip',
-    buttons: [
-        'copy', 'csv', 'excel', 'pdf', 'print'
-    ]
-} );
+
 
 
    
